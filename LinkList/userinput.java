175707630bc526f873a1be8a851320b668909b72
ankit <jayswalankit94@gmail.com>
2025-05-08 17:40:41 +0530

    completed it

--- LinkList/userinput.java
+++ LinkList/userinput.java
@@ -1,11 +1,13 @@
 
 import java.util.*;
 
-class Node{
+class Node
+{
     int data;
     Node address;
 
-    Node(int data){
+    Node(int data)
+    {
         this.data=data;
         this.address=null;
     }
@@ -15,17 +17,46 @@ public class userinput {
      Node head;
 
      public void add(int data){
+
         Node obj=new Node(data);
-        if(head==null){
+
+        if(head==null)
+        {
        head= obj;
         }
-        else{
+
+        else
+        {
             Node tem=head;
 
-            while(tem.address!=null){
+            while(tem.address!=null)
+            {
                   tem=tem.address;
             }
             tem.address=obj;
         }
+     }
+
+     public void display()
+     {
+        Node tem=head;
+        while(tem!=null){
+            System.out.println(tem.data);
+            tem=tem.address;
+        }
+     }
+     public static void main(String[] args)
+      {
+        Scanner sc=new Scanner(System.in);
+        userinput obj=new userinput();
+        System.out.println("Enter your size");
+        int size=sc.nextInt();
+       
+        for(int i=0;i<size;i++)
+        {
+            obj.add(sc.nextInt());
+        }
+        System.out.println("Display: ");
+        obj.display();
      }
 }
